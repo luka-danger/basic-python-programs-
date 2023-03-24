eaedbf991de293f03f00628a7c5b345410f02d4c
luka.danger <lukathevegan@gmail.com>
2023-03-24 17:03:35 -0600

    Initial Commit
    Another Udemy assignment. Don't forget to use float

---
+++ calculators/tip_calc.py
@@ -0,0 +1,11 @@
+print("Welcome to the tip calculator! Don't forget to tip your server! ")
+
+bill = float(input("What was the total bill?: ")) 
+tip = float(input("What percentage tip would you like to give? 18, 20, or 22? "))
+split = float(input("How many people to split the bill? "))
+percent = (0.01 * float(tip)) + 1 
+total = round((bill * percent) / split , 2) 
+total_tip = (total - bill)
+format_total_tip = float(total_tip)
+
+print(f'Each person should tip ${format_total_tip}, for a total of ${total}')
