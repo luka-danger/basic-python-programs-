62eb6792765abfcd072bab6861a80459454dc14c
luka.danger <lukathevegan@gmail.com>
2023-03-26 13:05:46 -0600

    Add ASCII Art, Add Strings

--- games/slender_man.py
+++ games/slender_man.py
@@ -24,6 +24,20 @@ stages = ['''
 
 ''']
 
+stage_strings = ['''
+Slender Man has caught you. There is no escape.
+''', '''
+Slender Man is getting closer. 
+''','''
+Slender Man is getting closer. 
+''','''
+You are being stalked by Slender Man.
+''','''
+Uh oh! You have been noticed by Slender Man.
+''', '''
+There is an ominous feeling in the air. Be careful! 
+''']
+
 word_list = ["cat", "dog", "cow", "monkey", "bird", "bear"]
 secret_word = list(random.choice(word_list)) 
 print(secret_word) #use for testing, then delete
@@ -35,13 +49,15 @@ word_length = len(secret_word)
 for letter in range(word_length):
     display += "_"
 print(display)
+print(stage_strings[5])
 
 #Use while loop to run code until all letters have been guessed
 while "_" in display: 
     guess = input("Guess a letter: \n").lower()
     if letter != guess: 
         lives -= 1 
-        print(lives)
+        print(stages[lives]) 
+        print(stage_strings[lives])
 #Replace "_" with letter if guess matches letter in chosen_word 
     for position in range(word_length): 
         letter = secret_word[position]   
