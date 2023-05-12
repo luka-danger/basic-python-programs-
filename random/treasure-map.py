958ecd649443b5602c9a5d1de25fefc3d8d18d29
luka.danger <lukathevegan@gmail.com>
2023-05-12 09:35:29 -0600

    Add int() to variable
    The code was trying to read the input for the position variable as a string. I wrapped the input in int() to fix the problem

--- random/treasure-map.py
+++ random/treasure-map.py
@@ -5,7 +5,7 @@ row2 = ["⬜️","⬜️","️⬜️"]
 row3 = ["⬜️️","⬜️️","⬜️️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to hide your treasure?")
+position = int(input("Where do you want to hide your treasure?")) 
 
 string = str(position)
 coordinates = []
