77aea8cf39a407ff9adc6b07d497f8c855ba5810
luka.danger <lukathevegan@gmail.com>
2023-05-25 17:26:35 -0600

    Initial Commit
    Practicing some potential coding questions for interviews. I found a solution that seems to cover every number, but the solution feels sub optimal. Going to think through the logic a little bit more.

---
+++ random/prime.py
@@ -0,0 +1,17 @@
+import math
+
+num = int(input("Enter number: "))
+print(math.sqrt(num))
+
+# Any number less than 2 is not prime
+if num < 2:
+    print(f"{num} is not a prime number.")
+# 2 is a prime number
+elif num == 2:
+    print(f"{num} is a prime number.")
+# Any number divisble by 2, 3, or 5 is not prime
+elif num % 2 == 0 or num % 3 == 0 or num % 5 == 0:
+    print(f"{num} is not a prime number.")
+# Any number that doesn't fit above criteria will be a prime number
+else:
+    print(f"{num} is a prime number.")
