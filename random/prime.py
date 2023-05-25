e676dd935ff2657245faf79958e93bfde5503bd9
luka.danger <lukathevegan@gmail.com>
2023-05-25 17:32:10 -0600

    Update logic
    The initial logic excluded 5, so I added and operator to include 5 as a prime number. Readability is not great and the solution still feels suboptimal. Might work through this one with a partner and see what we can come up with.

--- random/prime.py
+++ random/prime.py
@@ -1,7 +1,6 @@
 import math
 
 num = int(input("Enter number: "))
-print(math.sqrt(num))
 
 # Any number less than 2 is not prime
 if num < 2:
@@ -10,7 +9,7 @@ if num < 2:
 elif num == 2:
     print(f"{num} is a prime number.")
 # Any number divisble by 2, 3, or 5 is not prime
-elif num % 2 == 0 or num % 3 == 0 or num % 5 == 0:
+elif num % 2 == 0 or num % 3 == 0 or num % 5 == 0 and num > 5:
     print(f"{num} is not a prime number.")
 # Any number that doesn't fit above criteria will be a prime number
 else:
