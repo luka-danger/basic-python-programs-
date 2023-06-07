c72fd29835722727c4af009a93be3a69eb516d81
luka.danger <lukathevegan@gmail.com>
2023-06-06 21:35:51 -0600

    Update comments and remove pseudocode

--- calculators/paint_area.py
+++ calculators/paint_area.py
@@ -1,12 +1,9 @@
-# Given random height and width, create function to 
-# calculate number of paint cans needed 
-# H x W = Square Meters
-# 1 can of paint per 5 sq m 
-# (H * W) / 5 = Number of cans of paint
-# Round up paint cans 
 import math
 
+
 def paint_calc():
+    # Calculates number of paint cans (Area / 5)
+    # Ceiling function rounds up to nearest integer
     paintCans = math.ceil((height * width) / 5)
     print(f'You need {paintCans} cans of paint.') 
 
