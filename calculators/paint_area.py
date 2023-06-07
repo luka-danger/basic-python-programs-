b5dae63085406a9d0fba0e79394faecc41b53649
luka.danger <lukathevegan@gmail.com>
2023-06-06 21:33:39 -0600

    Import math module and add ceiling function to round up

--- calculators/paint_area.py
+++ calculators/paint_area.py
@@ -4,9 +4,10 @@
 # 1 can of paint per 5 sq m 
 # (H * W) / 5 = Number of cans of paint
 # Round up paint cans 
+import math
 
 def paint_calc():
-    paintCans = (height * width) / 5
+    paintCans = math.ceil((height * width) / 5)
     print(f'You need {paintCans} cans of paint.') 
 
 height = int(input("Height of wall: "))
