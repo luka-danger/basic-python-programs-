a54ef41994653accbbcb46dd6b52f671df97dc30
luka.danger <lukathevegan@gmail.com>
2023-06-06 22:10:48 -0600

    Add additional if statement
    The program worked, excluding 1 and 0. I added an additional if statement to make sure that 1 and 0 were not being identified as prime numbers. I could probably change the for loop range, but this way works too.

--- random/prime_loop.py
+++ random/prime_loop.py
@@ -2,9 +2,12 @@ is_prime = True
 
 num = int(input("Enter number: \n"))
 for i in range(2, num):
-    if num % i == 0: 
+    if num % i == 0:
         is_prime = False
         print(f'{num} is not a prime number.')
         break
-if is_prime:
+if num < 2:
+    is_prime = False
+    print(f'{num} is not a prime number.')
+elif is_prime:
     print(f'{num} is a prime number.')
