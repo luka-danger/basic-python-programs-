2fee1b713fa871fc67bdd6ae59aa374f1ce6c0c6
luka.danger <lukathevegan@gmail.com>
2023-06-06 21:54:56 -0600

    Add additional if statement

--- random/prime_loop.py
+++ random/prime_loop.py
@@ -5,3 +5,6 @@ for i in range(2, num):
     if num % i == 0: 
         is_prime = False
         print(f'{num} is not a prime number.')
+        break
+if is_prime:
+    print(f'{num} is a prime number.')
