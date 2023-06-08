19def235bbaf918b143a041242936222a5803e79
luka.danger <lukathevegan@gmail.com>
2023-06-08 17:55:45 -0600

    Create caesar function
    This is just for practice. I think I like the other way better...moze...

--- random/caesar-cipher.py
+++ random/caesar-cipher.py
@@ -26,8 +26,32 @@ def decrypt():
             decipherText += originalLetter
     print(f'The decoded output is {decipherText}')
 
-while True: 
+def caesar():
+    cipherText = ""
+    for letter in text:
+        if letter in text: 
+            position = alphabet.index(letter)
+            if direction == "decode":
+                originalPosition = (position - shift) % 26
+                originalLetter = alphabet[originalPosition]
+                cipherText += originalLetter
+            elif direction == "encode":
+                newPosition = (position + shift) % 26
+                newLetter = alphabet[newPosition]
+                cipherText += newLetter
+
+            else: 
+                print("Invalid input.")
+    print(f'The output is {cipherText}')
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+caesar()
+
+while False: 
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = ""
     if direction == "encode":
         text = input("Type your message:\n").lower()
         shift = int(input("Type the shift number:\n"))
