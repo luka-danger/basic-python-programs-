4b86b29d1a043d26004175350f23d9a962fcb2da
luka.danger <lukathevegan@gmail.com>
2023-06-08 18:08:55 -0600

    Change functions to caesar

--- random/caesar-cipher.py
+++ random/caesar-cipher.py
@@ -1,31 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt():
-    cipherText = ""
-    # Iterate through each letter in the text
-    for letter in text:
-        if letter in alphabet:
-           position = alphabet.index(letter)
-           # Position + Shift establishes new position 
-           # Modulus keeps the range within 26 
-           newPosition = (position + shift) % 26 
-           newLetter = alphabet[newPosition]
-           # Add newLetter to empty cipherText string
-           # with each iteration of the for loop 
-           cipherText += newLetter
-    print(f'The encoded output is {cipherText}')
-
-def decrypt():
-    decipherText = ""
-    for letter in text:
-        if letter in text: 
-            position = alphabet.index(letter)
-            originalPosition = (position - shift) % 26
-            originalLetter = alphabet[originalPosition]
-            decipherText += originalLetter
-    print(f'The decoded output is {decipherText}')
-
 def caesar():
     cipherText = ""
     # Iterate through each letter in the text
