ed722b8052d90e9e60bc64c921a0dbaf11c438b8
luka.danger <lukathevegan@gmail.com>
2023-06-08 18:04:26 -0600

    Final product

--- random/caesar-cipher.py
+++ random/caesar-cipher.py
@@ -28,38 +28,34 @@ def decrypt():
 
 def caesar():
     cipherText = ""
+    # Iterate through each letter in the text
     for letter in text:
         if letter in text: 
             position = alphabet.index(letter)
-            if direction == "decode":
+            if direction == "encode":
+                # Position + Shift establishes new position 
+                # Modulus keeps the range within 26 
+                newPosition = (position + shift) % 26
+                newLetter = alphabet[newPosition]
+                # Add newLetter to empty cipherText string
+                # with each iteration of the for loop 
+                cipherText += newLetter
+            elif direction == "decode":
                 originalPosition = (position - shift) % 26
                 originalLetter = alphabet[originalPosition]
                 cipherText += originalLetter
-            elif direction == "encode":
-                newPosition = (position + shift) % 26
-                newLetter = alphabet[newPosition]
-                cipherText += newLetter
+            print(f'Decode Output: {cipherText}')
 
-            else: 
-                print("Invalid input.")
-    print(f'The output is {cipherText}')
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-caesar()
-
-while False: 
+while True: 
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = ""
     if direction == "encode":
         text = input("Type your message:\n").lower()
         shift = int(input("Type the shift number:\n"))
-        encrypt()
+        caesar()
     elif direction == "decode":
         text = input("Enter a message to decode:\n").lower()
         shift = int(input("Type the shift number:\n"))
-        decrypt()
+        caesar()
     else:
         print("Please enter a valid input.")
 
