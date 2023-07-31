915e81675a224b9e1a82201821b41be95aed5886
luka.danger <lukathevegan@gmail.com>
2023-07-31 13:28:06 -0600

    Create for loop
    This is not the final draft of the loop, but starting to move in the right direction. The end goal is for the scores to be converted to grades and then move these values into the dictionary student_grades, along with their key pairs

--- random/grading-program.py
+++ random/grading-program.py
@@ -5,3 +5,12 @@ student_scores = {
     "Draco": 74,
     "Neville": 62
 }
+
+student_grades = {}
+for score in student_scores:
+    grade = student_scores[score]
+    if grade >= 91:
+        grade = "Outstanding"
+        print(grade)
+    else: 
+        print(grade)
