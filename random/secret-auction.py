b16736eb8e0769c7ad8a523b7f57836162267268
luka-danger <lukathevegan@gmail.com>
2023-08-02 13:17:47 -0600

    Update findMaxBid function

--- random/secret-auction.py
+++ random/secret-auction.py
@@ -3,12 +3,13 @@ bidders = []
 def secretAuction():
     newBid = {}
     newBid["name"] = input("What is your name?: ")
-    newBid["bid_amount"] = input("What is your bid?: ")
+    newBid["bid_amount"] = float(input("What is your bid?: ")) 
     bidders.append(newBid) 
 
 def findMaxBid():
-    maxBid = max(bid['bid_amount'] for bid in bidders) 
-    nameOfWinner = max(bidder['name'] for bidder in bidders) 
+    maxBid = max(bid["bid_amount"] for bid in bidders) 
+    winners = [bidder["name"] for bidder in bidders if bidder["bid_amount"] == maxBid]
+    nameOfWinner = "".join(winners)
     print(f'The winner is {nameOfWinner} with a bid of ${maxBid}!')
 
 secretAuction()
@@ -16,6 +17,7 @@ while True:
     moreBidders = input("Are there anymore bidders? Yes or No: ")
     if moreBidders.lower() == "yes":
         secretAuction()
+        print(bidders)
     elif moreBidders.lower() == "no": 
         print("No more bidders")
         findMaxBid()
