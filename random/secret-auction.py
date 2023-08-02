d20d7b4826256fa34ee1714102aade54cb6388c0
luka-danger <lukathevegan@gmail.com>
2023-08-02 11:44:47 -0600

    Create new if else statements

--- random/secret-auction.py
+++ random/secret-auction.py
@@ -11,7 +11,9 @@ while True:
     moreBidders = input("Are there anymore bidders? Yes or No: ")
     if moreBidders == "Yes":
         secretAuction()
-    else: 
+    elif moreBidders == "No": 
         print("No more bidders")
         break 
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
 
