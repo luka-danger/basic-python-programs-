44e78433a7f2f77009989ea36d2b401f80e65f8f
luka-danger <lukathevegan@gmail.com>
2023-08-01 20:56:06 -0600

    Initial commit

---
+++ random/co14er-tracker.py
@@ -0,0 +1,13 @@
+co14erTracker = [
+    {
+    "peak": "Quandary Peak", 
+     "total_summits": 1,
+     "range": "Tenmile",
+     "class": 1
+     } , 
+     {"peak": "Mt. Bierstadt",
+      "total_summits": 2,
+      "range": "Front",
+      "class": 2
+      }
+]
