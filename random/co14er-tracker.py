cb75868df78b8a08c3278fd2bfea14a45c694870
luka-danger <lukathevegan@gmail.com>
2023-08-01 21:10:14 -0600

    Add .append operator

--- random/co14er-tracker.py
+++ random/co14er-tracker.py
@@ -18,7 +18,9 @@ def addPeak(peak, totalSummits, range, classRank):
     newPeak["total_summits"] = totalSummits
     newPeak["range"] = range
     newPeak["class"] = classRank
+    co14erTracker.append(newPeak)
 
 addPeak("Longs", 1, "Front", 3)
+addPeak("Crestone Needle", 1 ,"Sangre De Cristo", 3)
 
 print(co14erTracker)
