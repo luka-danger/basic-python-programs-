7a0bcb04cab980134b4c7adeb4378ab070afe97d
luka-danger <lukathevegan@gmail.com>
2023-08-02 13:24:53 -0600

    Add comments

--- random/secret-auction/secret-auction.py
+++ random/secret-auction/secret-auction.py
@@ -1,14 +1,19 @@
 bidders = []
 
+# Ask users for name and bid amount 
 def secretAuction():
     newBid = {}
     newBid["name"] = input("What is your name?: ")
     newBid["bid_amount"] = float(input("What is your bid?: ")) 
+    # Add new bid to the bidders list 
     bidders.append(newBid) 
 
 def findMaxBid():
+    # Find max bid_amount in list of bidders
     maxBid = max(bid["bid_amount"] for bid in bidders) 
+    # Find name of bidder associated with maxBid 
     winners = [bidder["name"] for bidder in bidders if bidder["bid_amount"] == maxBid]
+    # Change list item to string 
     nameOfWinner = "".join(winners)
     print(f'The winner is {nameOfWinner} with a bid of ${maxBid}!')
 
