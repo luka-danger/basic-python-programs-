7aec9f699814674b9dfab4e0866ad5a9f6c0f1a2
luka-danger <lukathevegan@gmail.com>
2023-08-01 21:00:30 -0600

    Create new function

--- random/co14er-tracker.py
+++ random/co14er-tracker.py
@@ -11,3 +11,11 @@ co14erTracker = [
       "class": 2
       }
 ]
+
+def addPeak(): 
+    enterPeak = input("Enter a new peak: "); 
+    totalSummits = input("How many times have you climbed this peak?: "); 
+    enterRange = input("Which range is this peak in?: "); 
+    enterClass = input("What class ranking is this peak?: ")
+    
+
