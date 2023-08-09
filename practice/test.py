ad9aae5d227f5adc3adbd1e8c2494939362980d1
luka-danger <lukathevegan@gmail.com>
2023-08-09 09:54:44 -0600

    Initial commit
    Create practice file directory. This will be a mess of practice programs trying out different functions, methods, etc

---
+++ practice/test.py
@@ -0,0 +1,10 @@
+def multiply(first_num, second_num):
+    result = first_num * second_num
+    return result 
+
+def format_name(f_name, l_name):
+    full_name = f_name.title() + " " + l_name.title()
+    return full_name
+
+print(multiply(12, 17))
+print(format_name("nAtE", "edgE")) 
