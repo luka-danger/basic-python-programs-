e882e19d4742f7ce0d7c36f64585c47e8f156189
luka-danger <lukathevegan@gmail.com>
2023-08-09 12:07:03 -0600

    Add comments

--- practice/days-in-month.py
+++ practice/days-in-month.py
@@ -13,10 +13,14 @@ def is_leap(year):
 
 def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
-    for days in month_days: 
+    # Iterate through days in the month_days list
+    for days in month_days:
+        # Check if the month is 2 (February) during a leap year  
+        # If it is a leap year and february, the function returns 29 days
         if is_leap(year) == True and month == 2: 
            days = 29
         else: 
+            # Use month - 1 to prevent 'off by one' error 
             days = month_days[month - 1]
     return days 
   
