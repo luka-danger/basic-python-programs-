bf581d864ce6a20f07297fe7ebfc3b44bfecfcc2
luka-danger <lukathevegan@gmail.com>
2023-08-09 11:55:25 -0600

    Create new function
    The function works, but needs cleaner syntax

--- practice/days-in-month.py
+++ practice/days-in-month.py
@@ -11,12 +11,19 @@ def is_leap(year):
     return False 
   
 
-''' def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+def days_in_month(month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+    for days in month_days: 
+        if is_leap(2000) == True: 
+           days = 29
+        else: 
+            days = month_days[month - 1]
+    print(days)
+
+
   
   
-#🚨 Do NOT change any of the code below 
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
+#🚨 Do NOT change any of the code below
+days_in_month(2)
+''' days = days_in_month(year, month)
 print(days) ''' 
