07f7389f07a6f136f95a5edbc2f6a39b0ff81853
luka-danger <lukathevegan@gmail.com>
2023-08-09 11:32:24 -0600

    Change output

--- practice/days-in-month.py
+++ practice/days-in-month.py
@@ -2,15 +2,16 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True 
       else:
-        print("Not leap year.")
+        return False 
     else:
-      print("Leap year.")
+      return True 
   else:
-    print("Not leap year.")
+    return False 
+  
 
-def days_in_month():
+''' def days_in_month():
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
   
   
@@ -18,4 +19,4 @@ def days_in_month():
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
-print(days)
+print(days) ''' 
