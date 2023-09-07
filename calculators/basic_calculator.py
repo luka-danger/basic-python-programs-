bce0954632238ea67db3bdf199c56bd51a8ad4a4
luka-danger <lukathevegan@gmail.com>
2023-09-07 12:18:24 -0600

    Add strip()
    Python was reading the inputted + with a leading space, causing a KeyError. Strip removes the space and the program runs as intended

--- calculators/basic_calculator.py
+++ calculators/basic_calculator.py
@@ -1,5 +1,3 @@
-# Calculator Program
- 
 def add(n1, n2):
     return n1 + n2
 
@@ -12,19 +10,21 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-calculator = {
-    "+": add, 
-    "-": subtract, 
-    "*": multiply,
-    "/": divide
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
 }
 
 first_num = int(input("Enter first integer: ")) 
 second_num = int(input("Enter second integer: ")) 
-for symbol in calculator: 
+for symbol in operations: 
     print(symbol)
 operator = input("Choose an operator from the line above: ")
-calc_function = calculator[operator] 
+# Remove any leading or trailing spaces in operator 
+operator = operator.strip()
+calc_function = operations[operator]
 answer = calc_function(first_num, second_num)
 
 print(f'{first_num} {operator} {second_num} = {answer}' )
