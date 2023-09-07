72750c7cf9813338cf95122911bc7ff0584988d7
luka-danger <lukathevegan@gmail.com>
2023-09-07 13:50:15 -0600

    Create calculator function

--- calculators/basic_calculator.py
+++ calculators/basic_calculator.py
@@ -17,28 +17,29 @@ operations = {
   "/": divide
 }
 
-first_num = int(input("Enter first integer: ")) 
-for symbol in operations: 
-    print(symbol) 
-should_continue = True 
+def calculator(): 
+    first_num = int(input("Enter first integer: ")) 
+    for symbol in operations: 
+        print(symbol) 
+    should_continue = True 
 
-    
-while should_continue:
-    operator = input("Choose an operator from the line above: ") 
-    # Remove any leading or trailing spaces in operator 
-    operator = operator.strip()
-    second_num = int(input("Enter next integer: ")) 
-    calc_function = operations[operator]
-    answer = calc_function(first_num, second_num)
-    
-    print(f'{first_num} {operator} {second_num} = {answer}' )
-    
-    keep_calculating = input(f"Type 'y' to continue calculating with {answer} or \
-type 'n' to exit: ")
-    if keep_calculating == 'y': 
-        first_num = answer 
-    elif keep_calculating == 'n': 
-        should_continue = False 
+    while should_continue:
+        operator = input("Choose an operator from the line above: ") 
+        # Remove any leading or trailing spaces in operator 
+        operator = operator.strip()
+        second_num = int(input("Enter next integer: ")) 
+        calc_function = operations[operator]
+        answer = calc_function(first_num, second_num)
+        
+        print(f'{first_num} {operator} {second_num} = {answer}' )
+        
+        keep_calculating = input(f"Type 'y' to continue calculating with {answer} or \
+type 'n' to start a new calculator: ")
+        if keep_calculating == 'y': 
+            first_num = answer 
+        elif keep_calculating == 'n': 
+            should_continue = False 
+            calculator()
 
-
+calculator()
     
