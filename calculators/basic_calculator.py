ee24d17347b91ad96331f7ef033d424d0ba7b394
luka-danger <lukathevegan@gmail.com>
2023-09-07 10:29:35 -0600

    Initial commit

---
+++ calculators/basic_calculator.py
@@ -0,0 +1,19 @@
+# Calculator Program
+ 
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+def square_root(n1):
+    return n1 * n1 
+
+
+
