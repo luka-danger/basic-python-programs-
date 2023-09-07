2f552995c04141622c08f35e489be9be45165914
luka-danger <lukathevegan@gmail.com>
2023-09-07 14:12:06 -0600

    Add exponent function

--- calculators/basic_calculator/basic_calculator.py
+++ calculators/basic_calculator/basic_calculator.py
@@ -12,11 +12,15 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+def exponent(n1, n2):
+    return n1 ** n2 
+
 operations = {
   "+": add,
   "-": subtract,
   "*": multiply,
-  "/": divide
+  "/": divide,
+  "**": exponent
 }
 
 def calculator(): 
