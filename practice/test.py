9bcc7df05d6c0e36950ea37de43a54d143fc30ba
luka-danger <lukathevegan@gmail.com>
2023-09-07 07:48:38 -0600

    Practice functions

--- practice/test.py
+++ practice/test.py
@@ -8,3 +8,7 @@ def format_name(f_name, l_name):
 
 print(multiply(12, 17))
 print(format_name("nAtE", "edgE")) 
+
+ask_name = input("First Name?: ")
+ask_last_name = input ("Last Name?: ")
+print(format_name(ask_name, ask_last_name))
