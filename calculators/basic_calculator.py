96f805cf5648aee201a2a414a2b84cf731df2d84
luka-danger <lukathevegan@gmail.com>
2023-09-07 11:16:58 -0600

    Create function
    The + and * operators result in a key error

--- calculators/basic_calculator.py
+++ calculators/basic_calculator.py
@@ -12,20 +12,20 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-def square_root(n1):
-    return n1 * n1 
-
 calculator = {
     "+": add, 
     "-": subtract, 
     "*": multiply,
-    "/": divide, 
-    "^": square_root
-    }
+    "/": divide
+}
 
-first_num = input("Enter first integer: ")
-second_num = input("Enter second integer: ")
-math_function = input("Choose an operator: ")
-for operator in calculator: 
-    new_function = operator(first_num, second_num)
-    print(new_function)
+first_num = int(input("Enter first integer: ")) 
+second_num = int(input("Enter second integer: ")) 
+for symbol in calculator: 
+    print(symbol)
+operator = input("Choose an operator from the line above: ")
+calc_function = calculator[operator] 
+answer = calc_function(first_num, second_num)
+
+print(f'{first_num} {operator} {second_num} = {answer}' )
+    
