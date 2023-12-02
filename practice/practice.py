7ce6a92923687d2ec39533db71775fd71f049bab
luka-danger <lukathevegan@gmail.com>
2023-12-01 17:26:44 -0700

    Initial commit

---
+++ practice/practice.py
@@ -0,0 +1,15 @@
+# While Loop
+n = 1
+while n <= 5: 
+   print(n)
+   n += 1
+
+# For Loops 
+for i in range(5):
+   print("hi" * i)
+
+# (start, stop, decrement) 
+for i in range(5, 0, -1):
+    print(i)
+
+
