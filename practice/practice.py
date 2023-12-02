0a82409315d0e9d6c5b0005cee50eb9fbc2295ff
luka-danger <lukathevegan@gmail.com>
2023-12-01 18:15:35 -0700

    Re add file

---
+++ practice/practice.py
@@ -0,0 +1,41 @@
+# While Loop
+n = 1
+while n <= 5: 
+   print(n)
+   n += 1
+
+# For Loops 
+for i in range(5):
+   print("hi" * i)
+
+# (start, stop, decrement) 
+for i in range(5, 0, -1):
+    print(i)
+
+# division 
+print(5 / 2)
+# division rounded down
+print(5 // 2)
+# division with negative numbers 
+print(-3 / 2)
+
+# Modulo 
+print(10 % 3)
+
+# Negative Value Mod
+import math
+print(math.fmod(-10, 3))
+
+# Math Helper 
+print(math.floor(3/2))
+print(math.ceil(3/2))
+print(math.sqrt(25))
+print(math.pow(2,3))
+
+float("inf")
+float("-inf")
+print(math.pow(2,200)) 
+print(math.pow(2,200) < float("inf"))
+
+
+
