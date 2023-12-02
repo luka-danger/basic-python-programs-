d49988f56883daa21af6e13967e352e55e8b3b00
luka-danger <lukathevegan@gmail.com>
2023-12-01 17:40:17 -0700

    Initial commit

---
+++ practice/array-practice.py
@@ -0,0 +1,17 @@
+# Arrays (Called Lists in Python)
+arr = [1, 2, 3]
+print(arr)
+
+# Can be used as a stack 
+arr.append(4)
+arr.append(5)
+print(arr)
+
+# Combine lists
+arrgghh = ['a', 'b', 'c']
+arr.extend(arrgghh)
+print(arr)
+
+# Remove last value 
+arr.pop()
+print(arr)
