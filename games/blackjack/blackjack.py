93270f249a1e7757ecc685552e4d8cdcb6f9b5ea
luka-danger <lukathevegan@gmail.com>
2023-12-28 13:49:21 -0700

    Create Initial Deal, Hit, and Calculate Score Functions

--- games/blackjack/blackjack.py
+++ games/blackjack/blackjack.py
@@ -6,10 +6,21 @@ user_cards = []
 
 computer_cards = []
 
-def deal_card():
+def initial_deal():
     user_cards.append(random.choices(cards, k=2))
     computer_cards.append(random.choices(cards, k=2))
     print(f'Your cards: {user_cards}')
     print(f'Computer cards: {computer_cards}')
-    
-deal_card()
+
+def hit():
+    user_cards.append(random.choice(cards))
+    print(f'Cards: {user_cards}')
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
