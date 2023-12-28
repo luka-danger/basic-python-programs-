e655ce159bd57a0588e74b96692440e385fdc061
luka-danger <lukathevegan@gmail.com>
2023-12-27 23:27:34 -0700

    Create while loop

--- games/blackjack/blackjack.py
+++ games/blackjack/blackjack.py
@@ -1,1 +1,14 @@
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def play_game():
+    print('Playing game!')
+
+while True: 
+    start_game = input('Do you want to play blackjack? (y) or (n):\n') 
+    if start_game == 'y':
+        play_game()
+    elif start_game == 'n':
+        print('Boring!')
+        break 
+    else:
+        print('Please enter valid option')
