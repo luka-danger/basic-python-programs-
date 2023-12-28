2407fef429e2b329e3f52d41d8e3a125ee147349
luka-danger <lukathevegan@gmail.com>
2023-12-27 23:42:05 -0700

    Create deal card function

--- games/blackjack/blackjack.py
+++ games/blackjack/blackjack.py
@@ -1,12 +1,20 @@
+import random
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def play_game():
-    print('Playing game!')
+user_cards = []
+
+computer_cards = []
+
+def deal_card():
+    deal_card = random.choice(cards)
+    return deal_card
+    
 
 while True: 
     start_game = input('Do you want to play blackjack? (y) or (n):\n') 
     if start_game == 'y':
-        play_game()
+        deal_card()
     elif start_game == 'n':
         print('Boring!')
         break 
