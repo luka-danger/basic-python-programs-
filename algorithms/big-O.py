3c19a05692c71c6e8d98e704ee9e2de96da37429
luka-danger <lukathevegan@gmail.com>
2024-01-10 13:49:47 -0700

    Add O(n^2) algo

--- algorithms/big-O.py
+++ algorithms/big-O.py
@@ -6,3 +6,18 @@ def print_items(n):
         print(i)
 
 print_items(10)
+
+# Drop Constants O(n)
+# n + n = n 
+def print_items(n):
+    for i in range(n):
+        print(i)
+    for j in range(n):
+        print(j)
+
+# O(n^2)
+# nxn = n^2
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j) 
