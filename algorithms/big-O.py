f53b53c4cc9cca21b512af646feda8492fbfd70f
luka-danger <lukathevegan@gmail.com>
2024-01-10 13:54:10 -0700

    Add O(1)

--- algorithms/big-O.py
+++ algorithms/big-O.py
@@ -20,4 +20,24 @@ def print_items(n):
 def print_items(n):
     for i in range(n):
         for j in range(n):
-            print(i,j) 
+            print(i,j)
+
+# Drop non-dominants
+#O(n^2 + n) = O(n^2)
+
+# O(n^2)
+def print_items(n):
+    #O(n)
+    for i in range(n):
+        #O(n)
+        for j in range(n):
+            print(i,j)
+        #O(n)
+        for k in range(n):
+            print(k)
+    
+# O(1)
+# Number of operations don't change for addition
+# n + n + n is also O(1)
+def add_items(n):
+    return n + n
