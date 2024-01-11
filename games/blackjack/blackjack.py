d1123673b05c0b46ed3b10fae6baf60557a53d1f
luka-danger <lukathevegan@gmail.com>
2024-01-10 19:54:13 -0700

    Create second while loop
    To calculate computer score and continue drawing cards

--- games/blackjack/blackjack.py
+++ games/blackjack/blackjack.py
@@ -37,3 +37,7 @@ while not is_game_over:
         user_cards.append(deal_card())
     else:
         is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_score.append(deal_card())
+    computer_score = calculate_score(computer_cards)
