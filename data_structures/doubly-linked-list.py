d38a7ae5d504f5ca227a4619dcc2cba41d49997b
luka-danger <lukathevegan@gmail.com>
2024-01-15 15:06:46 -0700

    Create linked list constructor

--- data_structures/doubly-linked-list.py
+++ data_structures/doubly-linked-list.py
@@ -6,3 +6,20 @@ class Node:
         self.next = None
         self.prev = None
 
+# Doubly linked list constructor 
+class DoublyLinkedList: 
+    def __init__(self, value): 
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+my_doubly_linked_list = DoublyLinkedList(7)
+
+my_doubly_linked_list.print_list()
