aa13e22769b7d6cd5f6e00ef05816149e7fa52a4
luka-danger <lukathevegan@gmail.com>
2024-01-15 15:49:14 -0700

    Add else statement

--- data_structures/doubly-linked-list.py
+++ data_structures/doubly-linked-list.py
@@ -58,9 +58,10 @@ class DoublyLinkedList:
         if self.length == 0:
             self.head = new_node
             self.tail = new_node
-        new_node.next = self.head
-        self.head.prev = new_node
-        self.head = new_node
+        else: 
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
         self.length += 1
         return True 
 
