18481f3fb057acda46a520f1c0ad33ce080d0877
luka-danger <lukathevegan@gmail.com>
2024-01-15 15:45:55 -0700

    Create prepend method

--- data_structures/doubly-linked-list.py
+++ data_structures/doubly-linked-list.py
@@ -52,10 +52,22 @@ class DoublyLinkedList:
             temp.prev = None 
         self.length -= 1
         return temp 
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
+        self.length += 1
+        return True 
 
             
 my_doubly_linked_list = DoublyLinkedList(7)
 my_doubly_linked_list.append(8)
 my_doubly_linked_list.pop()
+my_doubly_linked_list.prepend(5)
 
 my_doubly_linked_list.print_list()
