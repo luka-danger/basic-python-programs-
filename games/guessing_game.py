6fe9372f4e6429d61d37aebd8629cf8b27a3305b
luka-danger <lukathevegan@gmail.com>
2024-01-21 18:43:23 -0700

    Update UI
    Add additional print statement and edit input statement

--- games/guessing_game.py
+++ games/guessing_game.py
@@ -6,8 +6,9 @@ secret_number = random.randint(1, 100 + 1)
 guess_count = 0
 #Limit number of guesses to 7
 guess_limit = 7
+print('Guess a number between 1 and 100')
 while guess_count < guess_limit:
-    guess = int(input("Guess: "))
+    guess = int(input("Your Guess: "))
     #Add one count to guess each time 
     guess_count += 1
     if guess > secret_number:
