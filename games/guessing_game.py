f5d39ca087fd6b1a68781443e77c0ae62793bc11
luka-danger <lukathevegan@gmail.com>
2024-01-21 20:12:18 -0700

    Add final code

--- games/guessing_game.py
+++ games/guessing_game.py
@@ -1,10 +1,3 @@
-# Choose random num 1 - 100
-# Make function to set difficulty
-# Let user guess number
-# Make function to check user guess against actual answer
-# Track number of turns and reduce by 1 if wrong
-# Repeat guessing game if guess is wrong 
-
 import random 
 
 secret_number = random.randint(1, 100)
@@ -16,35 +9,35 @@ print("I'm thinking of a number between 1 and 100")
 def set_difficulty():
     difficulty = input('Choose a difficulty. Type easy or hard: \n')
     if difficulty.casefold() == 'Hard'.casefold():
-        return guess_count - 5
+        global guess_count
+        guess_count -= 5
     elif difficulty.casefold() == 'Easy'.casefold():
         return guess_count
-    # Prevent invalid user input
-    try:  
+    else:  
         print('Please enter a valid input.')
-    except ValueError:
-        print('Please enter a valid input.')
+   
 
 def check_guess():
     if guess == secret_number: 
-        print(f'You win! The number I was thinking was {secret_number}')
+        print(f'You win! {secret_number} is the correct number.')
     elif guess > secret_number:
-        print('Too high!')
+        print('Too high.')
     elif guess < secret_number: 
-        print('Too low')
-    elif guess_count == 0:
-        print(f'You lose! The number I was thinking of was {secret_number}')
+        print('Too low.')
     else: 
         print('Please enter a valid integer: ')
-    return guess_count - 1
 
 set_difficulty()
-print(guess_count)
+
 while guess_count != 0:
-    print(f'You have {guess_count} attempts remaining to guess a number')
-    guess = int(input('Make a guess: \n')) 
+    print(f'You have {guess_count} attempts remaining to guess a number.')
+    guess = int(input('Make a guess: ')) 
     check_guess()
-    
-    
+    guess_count -= 1
+    if guess == secret_number:
+        break 
+
+if guess_count == 0:
+        print(f'You lose! The correct number was {secret_number}.')
 
     
