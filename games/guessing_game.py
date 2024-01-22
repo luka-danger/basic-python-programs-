0268237ed22457df204c4c551720bfb4ecc25f60
luka-danger <lukathevegan@gmail.com>
2024-01-21 18:49:03 -0700

    Refactor code
    Remaking this game, hopefully improving the code a little bit. Cool

--- games/guessing_game.py
+++ games/guessing_game.py
@@ -1,23 +1,5 @@
 import random 
 
-#Assign random number between 1 and 100
-secret_number = random.randint(1, 100 + 1)
-#Set guess count to 0
-guess_count = 0
-#Limit number of guesses to 7
-guess_limit = 7
-print('Guess a number between 1 and 100')
-while guess_count < guess_limit:
-    guess = int(input("Your Guess: "))
-    #Add one count to guess each time 
-    guess_count += 1
-    if guess > secret_number:
-        print("Guess lower")
-    elif guess < secret_number:
-        print("Guess higher")
-    elif guess == secret_number:
-        print('You won')
-        break 
-    elif guess_count == guess_limit:
-        print('You lose')
-        print(secret_number) 
+secret_number = random.randint(1, 100)
+
+print(secret_number)
