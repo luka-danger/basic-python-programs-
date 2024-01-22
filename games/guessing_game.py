1f00c6404a60a8d0fdf0d551bdaeef3b536ef1f2
luka-danger <lukathevegan@gmail.com>
2024-01-21 19:31:53 -0700

    Create set difficulty function

--- games/guessing_game.py
+++ games/guessing_game.py
@@ -1,3 +1,11 @@
+# Choose random num 1 - 100
+# Make function to set difficulty
+# Let user guess number
+# Make function to check user guess against actual answer
+# Track number of turns and reduce by 1 if wrong
+# Repeat guessing game if guess is wrong 
+
+
 import random 
 
 secret_number = random.randint(1, 100)
@@ -6,14 +14,27 @@ guess_count = 10
 print("Welcome to the guessing game!")
 print("I'm thinking of a number between 1 and 100")
 
-while guess_count != 0: 
+def set_difficulty():
     difficulty = input('Choose a difficulty. Type easy or hard: \n')
-    if difficulty.casefold() == 'Hard'.casefold():
-        guess_count -= 5
-        print(guess_count)
-        break
-    elif difficulty.casefold() == 'Easy'.casefold():
-        print(guess_count)
-        break 
-    else: 
+    if difficulty.casefold() == 'Easy'.casefold():
+        return guess_count
+    elif difficulty.casefold() == 'Hard'.casefold():
+        return guess_count - 5 
+    # Prevent invalid user input
+    try:  
         print('Please enter a valid input.')
+    except ValueError:
+        print('Please enter a valid input.')
+
+
+set_difficulty()
+print(f'You have {guess_count} attempts remaining to guess a number')
+
+def make_guess():
+    print(f'You have {guess_count} attempts remaining to guess a number')
+    guess = ('Make a guess: \n')
+    if guess > secret_number:
+        print('Too high!')
+    
+
+    
