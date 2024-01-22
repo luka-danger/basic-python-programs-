e97ed3c7f1496c34c671f854e567fa9c9186e620
luka-danger <lukathevegan@gmail.com>
2024-01-21 19:45:56 -0700

    Create more functions
    Idk man I got distracted listening to some vibey music and writing code and forgot to commit earlier

--- games/guessing_game.py
+++ games/guessing_game.py
@@ -5,7 +5,6 @@
 # Track number of turns and reduce by 1 if wrong
 # Repeat guessing game if guess is wrong 
 
-
 import random 
 
 secret_number = random.randint(1, 100)
@@ -16,25 +15,36 @@ print("I'm thinking of a number between 1 and 100")
 
 def set_difficulty():
     difficulty = input('Choose a difficulty. Type easy or hard: \n')
-    if difficulty.casefold() == 'Easy'.casefold():
+    if difficulty.casefold() == 'Hard'.casefold():
+        return guess_count - 5
+    elif difficulty.casefold() == 'Easy'.casefold():
         return guess_count
-    elif difficulty.casefold() == 'Hard'.casefold():
-        return guess_count - 5 
     # Prevent invalid user input
     try:  
         print('Please enter a valid input.')
     except ValueError:
         print('Please enter a valid input.')
 
+def check_guess():
+    if guess == secret_number: 
+        print(f'You win! The number I was thinking was {secret_number}')
+    elif guess > secret_number:
+        print('Too high!')
+    elif guess < secret_number: 
+        print('Too low')
+    elif guess_count == 0:
+        print(f'You lose! The number I was thinking of was {secret_number}')
+    else: 
+        print('Please enter a valid integer: ')
+    return guess_count - 1
 
 set_difficulty()
-print(f'You have {guess_count} attempts remaining to guess a number')
-
-def make_guess():
+print(guess_count)
+while guess_count != 0:
     print(f'You have {guess_count} attempts remaining to guess a number')
-    guess = ('Make a guess: \n')
-    if guess > secret_number:
-        print('Too high!')
+    guess = int(input('Make a guess: \n')) 
+    check_guess()
+    
     
 
     
