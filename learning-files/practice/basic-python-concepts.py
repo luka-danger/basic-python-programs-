8252ba83f177993a1d8b9a8a5e761dbd067cd2b4
luka-danger <lukathevegan@gmail.com>
2024-01-24 11:55:06 -0700

    Add new practice file"

---
+++ learning-files/practice/basic-python-concepts.py
@@ -0,0 +1,19 @@
+a = range(5)
+for i in a:
+    print(i)
+
+names = {1: 'Ders', 2: 'Blake', 3: 'Adam'}
+
+print(f'{names} You gotta, gotta, gotta be fresh!')
+
+print(names[2])
+
+for key, value in names.items(): 
+    print(value)
+
+myList = [1, 2, 'fruit basket turnover', 4]
+for i in myList:
+    print(i)
+
+a = [[4, 5], [6, 7], [8, 9]]
+print(a[1][1])
